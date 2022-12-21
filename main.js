8422a25ce54891c7fc7ec8f38fd069e1e9a3f74c
Tundra Smooth <107771145+Angel-G03@users.noreply.github.com>
2022-12-21 18:35:30 -0500

    Getting save mode from radio buttons

--- main.js
+++ main.js
@@ -3,11 +3,22 @@ function upload() {
     console.log("Uploading files...");
 }
 
+function getSaveMode() {
+    var saveMode = document.getElementsByName('saveType');
+    for (var mode of saveMode) {
+        if (mode.checked) {
+            return mode.value
+        }
+    }
+    return null;
+}
+
 function crop() {
     var file = document.getElementById("upload").value;
     var top = document.getElementById("top").value;
     var bottom = document.getElementById("bottom").value;
     var left = document.getElementById("left").value;
     var right = document.getElementById("right").value;
-    console.log("Cropping " + file);
+    var saveMode = getSaveMode()
+    console.log("Cropping " + file + " by " + saveMode);
 }
