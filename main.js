d24a04649b2362b1260677f8648e4f5953b2b937
Tundra Smooth <107771145+Angel-G03@users.noreply.github.com>
2022-12-21 18:12:56 -0500

    Getting cropped dimensions

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+// This function isn't really neccesary but will stay for now
 function upload() {
     console.log("Uploading files...");
 }
 
 function crop() {
     var file = document.getElementById("upload").value;
+    var top = document.getElementById("top").value;
+    var bottom = document.getElementById("bottom").value;
+    var left = document.getElementById("left").value;
+    var right = document.getElementById("right").value;
     console.log("Cropping " + file);
 }
